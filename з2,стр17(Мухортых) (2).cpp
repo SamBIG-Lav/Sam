dd806cc8baac48d9d13f76782ee42ddeb04118d5
Sam <161628695+SamBIG-Lav@users.noreply.github.com>
2025-05-26 17:02:03 +0300

    Add files via upload

---
+++ з2,стр17(Мухортых) (2).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "¬ведите x: ";
+    cin >> x;
+
+    if (x <= 0) {
+        cout << "x должен быть больше 0" << endl;
+    }
+    else {
+
+        double sqrt_abs_x = sqrt(fabs(x));
+        double cos_sqrt_x = cos(sqrt_abs_x);
+        double cos6 = pow(cos_sqrt_x, 6);
+        double cos2 = pow(cos_sqrt_x, 2);
+        double fraction = cos6 / (cos2 + 5);
+        double arccos3 = pow(acos(fraction), 3);
+        double lg_2x = log10(2 * x);
+        double inner_log = pow(3, lg_2x) + 2;
+        double log3_val = log(inner_log) / log(3);
+        y = sqrt(arccos3 + log3_val);
+        cout << "y(" << x << ") = " << y << endl;
+    }
+
+    int N;
+    cout << "¬ведите N(трЄхзначное, натуральное): ";
+    cin >> N;
+
+    
+    if (N < 100 || N > 999) {
+        cout << "ќшибка" << endl;
+        return 1;
+    }
+
+    
+    int reversed = 0;
+    int temp = N;
+    while (temp > 0) {
+        int digit = temp % 10;
+        reversed = reversed * 10 + digit; 
+        temp /= 10; 
+    }
+
+    cout << "„исло с цифрами в обратном пор€дке: " << reversed << endl;
+
+    return 0;
+}
