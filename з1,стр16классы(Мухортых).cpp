3e3759a1af41f6b140984f9660c7810308ef443f
Sam <161628695+SamBIG-Lav@users.noreply.github.com>
2025-06-07 10:14:56 +0300

    Update з1,стр16классы(Мухортых).cpp

--- з1,стр16классы(Мухортых).cpp
+++ з1,стр16классы(Мухортых).cpp
@@ -1,66 +1,40 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
-class FunctionChecker {
-private:
+class MathFunction {
+    double x, y;
     
-    double computeY(double x) {
-        double sqrt_abs_x = sqrt(fabs(x));
-        double cos_sqrt_x = cos(sqrt_abs_x);
+public:
+    void inputX() {
+        cout << "Введите x: ";
+        cin >> x;
+    }
+    
+    void calculateY() {
+        if (x <= 0) {
+            cout << "False" << endl;
+            return;
+        }
+        
+        double cos_sqrt_x = cos(sqrt(x));
         double cos6 = pow(cos_sqrt_x, 6);
         double cos2 = pow(cos_sqrt_x, 2);
         double fraction = cos6 / (cos2 + 5);
         double arccos3 = pow(acos(fraction), 3);
         double lg_2x = log10(2 * x);
-        double inner_log = pow(3, lg_2x) + 2;
-        double log3_val = log(inner_log) / log(3);
-        return sqrt(arccos3 + log3_val);
-    }
-
-public:
-    
-    double calculateY(double x) {
-        return computeY(x);
-    }
-
-    
-    bool isInRegionD(double x, double y) {
+        double log3_val = log(pow(3, lg_2x) + 2) / log(3);
         
-        if (x <= 0) {
-            return false;
-        }
-
+        y = sqrt(arccos3 + log3_val);
         
-        double computed_y = computeY(x);
-        const double EPSILON = 1e-6; 
-        return fabs(y - computed_y) < EPSILON;
+        cout << "y(" << x << ") = " << y << endl;
+        cout << (y >= 0 && y <= 5 ? "True" : "False") << endl;
     }
 };
 
 int main() {
-    double x, y;
-
-    
-    FunctionChecker checker;
-
-   
-    std::cout << "Введите x: ";
-    std::cin >> x;
-
- 
-    y = checker.calculateY(x);
-    std::cout << y << std::endl;
-
-    std::cout << "Введите y: ";
-    std::cin >> y;
-
-    
-    if (checker.isInRegionD(x, y)) {
-        std::cout << "true" << std::endl;
-    }
-    else {
-        std::cout << "false" << std::endl;
-    }
-
+    MathFunction func;
+    func.inputX();
+    func.calculateY();
     return 0;
 }
