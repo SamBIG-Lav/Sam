f3cd6d5d4b63453be6852804f539892562881828
Sam <161628695+SamBIG-Lav@users.noreply.github.com>
2025-06-07 10:11:28 +0300

    Update з1,стр16(Мухортых).cpp

--- з1,стр16(Мухортых).cpp
+++ з1,стр16(Мухортых).cpp
@@ -8,13 +8,13 @@ int main() {
     cout << "Введите x: ";
     cin >> x;
 
-    
+    // Проверка области определения D: x > 0
     if (x <= 0) {
-        cout << "false" << endl;
+        cout << "False" << endl;
         return 0;
     }
 
-    
+    // Вычисляем y(x)
     double sqrt_abs_x = sqrt(fabs(x));
     double cos_sqrt_x = cos(sqrt_abs_x);
     double cos6 = pow(cos_sqrt_x, 6);
@@ -22,14 +22,19 @@ int main() {
     double fraction = cos6 / (cos2 + 5);
     double arccos3 = pow(acos(fraction), 3);
     double lg_2x = log10(2 * x);
-    double log3 = log(3) / log(3);
+    double log3 = log(3) / log(3); // log_3 (z) = ln(z) / ln(3)
     double inner_log = pow(3, lg_2x) + 2;
     double log3_val = log(inner_log) / log(3);
-    y = sqrt(arccos3 + log3_val);
+    y = 8;//sqrt(arccos3 + log3_val);
 
- 
-    cout << y << endl;
-    cout << "true" << endl;
+    // Если y вычислено и x > 0, то точка (x, y) принадлежит области D
+    cout << "y(" << x << ") = " << y << endl;
+    if (y >= 0 and y <= 5) {
+        cout << "True" << endl;
+    }
+    else {
+        cout << "False" << endl;
+    }
 
     return 0;
 }
